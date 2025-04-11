b1834650a731b72f0c82defc87880e71041a4404
Chris Smith <chris@iMac.lan>
2025-04-10 23:47:34 -0400

    MVP update to add author and some small style updates

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 const books = [
-  { title: "The Final Empire", date: "Jan 2025", rating: 5 },
-  { title: "The Way of Kings", date: "Feb 2025", rating: 4.5 },
-  { title: "Warbreaker", date: "Mar 2025", rating: 4 },
+  { title: "The Final Empire", author: "Brandon Sanderson", date: "Jan 2025", rating: 5 },
+  { title: "The Way of Kings", author: "Brandon Sanderson", date: "Feb 2025", rating: 4.5 },
+  { title: "Warbreaker", author: "Brandon Sanderson", date: "Mar 2025", rating: 4 },
 ];
 
 const list = document.getElementById("book-list");
 
 books.forEach(book => {
   const li = document.createElement("li");
-  li.className = "list-group-item d-flex justify-content-between align-items-center";
+  li.className = "list-group-item";
   li.innerHTML = `
-    <strong>${book.title}</strong>
-    <span>${book.date} &mdash; ⭐ ${book.rating}</span>
+    <div class="d-flex justify-content-between align-items-center">
+      <div>
+        <strong>${book.title}</strong><br>
+        <small class="text-muted">by ${book.author}</small>
+      </div>
+      <div>
+        <span>${book.date} &mdash; ⭐ ${book.rating}</span>
+      </div>
+    </div>
   `;
   list.appendChild(li);
 });
